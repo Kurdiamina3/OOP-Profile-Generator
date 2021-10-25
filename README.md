1bed6a92e64015d83a7008972e1078e37743f4ae
Kurdiamina3 <kurdiamina3@gmail.com>
2021-10-25 16:20:14 -0400

    team gen

---
+++ README.md
@@ -0,0 +1,52 @@
+
+  
+  # <h1 align="center"> 🌻 Team Profile Generator 🌻 <h1>
+  
+----
+
+<a href="https://img.shields.io/badge/License-MIT License-blueviolet"><img src="https://img.shields.io/badge/License-MIT License-blueviolet"></a>
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+
+----
+
+### Description
+This project is an application that generates a team profile with user input using Inquirer from Node.js. It uses the information and displays and html that displays the Manager, Engineer and Intern. This project demonstrates the uses of OOP and TDD.
+
+![Screenshot Demo](Demo Team Profile Generator)
+----
+### Installation
+🔧User should have Node.js downloaded. npm install, npm i inquirer
+
+----
+### Usage
+✏️node index to use inquirer to answer questions about your team. 
+
+----
+### License
+✅<a href="https://img.shields.io/badge/License-MIT License-blueviolet"><img src="https://img.shields.io/badge/License-MIT License-blueviolet"></a>
+This application is covered by the MIT License license.
+----
+
+### Contributing
+🤝N/A
+
+----
+### Test-Instructions
+Testing uses Jest from Node.js
+
+----
+### Questions
+💻Find me on GitHub: [Github Profile](https://github.com/kurdiamina3)
+<br />
+📧Email me with any questions: kurdiamina3@gmail.com 
+<br />
+
+🌟_This README was generated with 💓 by [README-generator] (https://github.com/kurdiamina3/README-generator)🌟
+
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const generateHTML = require('./src/generateHTML');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
 
 const fs = require('fs');
 
 const inquirer = require('inquirer');
 
-const teamArray = [];
+let teamArray = [];
 
 const addManager = () => {
   return inquirer.prompt ([
@@ -67,6 +68,7 @@ const addManager = () => {
       }
     }
   ])
+
   .then(managerInput => {
     const { name, id, email, officeNumber } = managerInput;
     const manager = new Manager (name, id, email, officeNumber);
@@ -76,4 +78,135 @@ const addManager = () => {
   })
 };
 
+const addEmployee = () => {
+  console.log (`
+    ================
+    Adding employees to the team
+    ================
+  `);
 
+  return inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'role',
+      message: "Please choose your employee's role.",
+      choices: ['Engineer', 'Intern']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: "What's the employee's name?",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log ("Please enter a name!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Please enter an employee ID.",
+      validate: nameInput => {
+        if (isNaN(nameInput)) {
+          console.log ("Please enter the employee's ID!")
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the employee's email.",
+      validate: email => {
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+        if (valid) {
+          return true;
+        } else { 
+          console.log ('Please enter an email!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: "Please enter the employee's github username.",
+      when: (input) => input.role === "Engineer",
+      validate: nameInput => {
+        if (nameInput ) {
+          return true;
+        } else {
+          console.log ("Please enter the employee's github username!")
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: "Please enter the Intern's school.",
+      when: (input) => input.role === "Intern",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log ("Please enter the intern's school.")
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddEmployee',
+      message: 'Would you like to add more team memebers?',
+      default: false
+    }
+  ])
+  
+  .then(employeeData => {
+    let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+    let employee;
+
+    if (role === "Engineer") {
+      employee = new Engineer (name, id, email, github);
+      console.log(employee);
+    } else if (role === "Intern") {
+      employee = new Intern (name, id, email, school);
+      console.log(employee);
+    }
+
+    teamArray.push(employee);
+
+    if (confirmAddEmployee) {
+      return addEmployee(teamArray);
+    } else {
+      return teamArray;
+    }
+  })
+};
+
+const writeFile = data => {
+  fs.writeFile ('./dist/index.html', data, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Your team profile has been successfully created! Check out the index.html")
+    }
+  })
+};
+
+addManager ()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML (teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+    console.log(err);
+  });
